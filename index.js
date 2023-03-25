de69220fcf1b660c7f21274c8b8250b2712d18a8
FabianUlises <ulisesfab.car@outlook.com>
2023-03-25 05:22:51 -0700

    Added users routes

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 // Dependencies
 const dotenv = require('dotenv').config();
 const express = require('express');
+const morgan = require('morgan');
 // Server configuration
 const app = express();
 // Middleware
 app.use(express.json());
+app.use(morgan('dev'));
 // Get requested time and date
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
@@ -12,6 +14,7 @@ app.use((req, res, next) => {
 });
 // Routes
 app.use('/api/v1/tours', require('./routes/tourRoutes'));
+app.use('/api/v1/users', require('./routes/userRoutes'));
 // Server on
 app.listen(process.env.PORT, (err) => {
     console.log(`server listening on port: ${process.env.PORT}`);
