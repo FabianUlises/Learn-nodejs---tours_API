c9d07bd6aa4c24d3e10b943ee37e189a05ae06b6
FabianUlises <ulisesfab.car@outlook.com>
2023-03-25 09:24:50 -0700

    Installed mongoose and connect to database

--- server.js
+++ server.js
@@ -1,4 +1,14 @@
 const app = require('./index');
+const mongoose = require('mongoose');
+// Mongoose db connection
+mongoose.connect(process.env.MONGO_URI,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then((conn) => {
+    console.log(`mongoose connected to db: ${process.env.MONGO_URI}`);
+}).catch((err) => {
+    console.log('error', err);
+})
 // Server Start
 app.listen(process.env.PORT, (err) => {
     console.log(`server listening on port: ${process.env.PORT}`);
