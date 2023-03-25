be0fe35795fc27d53c7b59c8cddb37ecde0187ae
FabianUlises <ulisesfab.car@outlook.com>
2023-03-25 05:28:56 -0700

    Created stub routes for users

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const router = express.Router();
 router.get('/', (req, res) => {
-    res.send('user stub route');
+    res.send('get users stub route');
+});
+router.post('/', (req, res) => {
+    res.send('users post stub route');
+});
+router.get('/:id', (req, res) => {
+    res.send('get user stub route');
+});
+router.patch('/:id', (req, res) => {
+    res.send('update user stub route');
+});
+router.delete('/:id', (req, res) => {
+    res.send('delete user stub route');
 });
 module.exports = router;
