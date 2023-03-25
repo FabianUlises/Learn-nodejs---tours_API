3372c612382053b20092d39dafacaee747ac10a4
FabianUlises <ulisesfab.car@outlook.com>
2023-03-25 04:44:14 -0700

    Added stub routes for delete and patch

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -7,4 +7,10 @@ router.post('/', (req, res) => {
     console.log(req.body);
     res.status(201).send('/api/v1/tours post route');
 });
+router.patch('/', (req, res) => {
+    res.status(200).send('/api/v1/tours patch route');
+});
+router.delete('/', (req, res) => {
+    res.status(204).send('/api/v1/tours delete route');
+});
 module.exports = router;
