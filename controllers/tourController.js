5eef76c73b4b89c99363ae7fd96ab2ba3ac1a5ad
FabianUlises <ulisesfab.car@outlook.com>
2023-03-25 13:45:59 -0700

    Seperated APIFeatures class into own file

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,46 +1,8 @@
 // Model
 const Tour = require('../models/tourModel');
 const devTours = require('../dev-data/data/tours');
-class APIFeatures {
-    constructor(query, queryString) {
-        this.query = query;
-        this.queryString = queryString;
-    }
-    filter() {
-        // Filtering
-        // Query
-        const queryObj = { ...this.queryString };
-        // Removing unwanted query items from query string
-        const excludedFields = ['page', 'sort', 'limit', 'fields'];
-        excludedFields.forEach((el) => delete queryObj[el]);
-        // Advanced filtering
-        let queryStr = JSON.stringify(queryObj);
-        queryStr = queryStr.replace(/\b(gte|gt|lte)\b/g, match => `$${match}`);
-        this.query = this.query.find(JSON.parse(queryStr));
-        return this;
-    }
-    sort() {
-        // Sorting
-        if(this.queryString.sort) {
-            const sortBy = this.queryString.sort.split(',').join(' ');
-            // Sorting query
-            this.query = this.query.sort(sortBy);
-        } else {
-            this.query = this.query.sort('price');
-        }
-        return this;
-    }
-    limitFields() {
-        // Limiting
-        if(this.queryString.fields) {
-            const fields = this.queryString.fields.split(',').join(' ');
-            this.query = this.query.select(fields);
-        } else {
-            this.query = this.query.select('-__v');
-        }
-        return this;
-    }
-};
+// Api features
+const APIFeatures = require('./../util/apiFeatures');
 // Get all tours
 exports.getAllTours = async (req, res) => {
     try {
