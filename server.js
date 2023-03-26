71333bdd42ba15ae35aab58621ad4c0e731a99e1
FabianUlises <ulisesfab.car@outlook.com>
2023-03-26 06:57:40 -0700

    Added eventlistener to handle unhandled rejections

--- server.js
+++ server.js
@@ -7,9 +7,15 @@ mongoose.connect(process.env.MONGO_URI,{
 }).then((conn) => {
     console.log(`mongoose connected to db: ${process.env.MONGO_URI}`);
 }).catch((err) => {
-    console.log('error', err);
-})
+    console.log('ERROR', err);
+});
 // Server Start
-app.listen(process.env.PORT, (err) => {
+const server = app.listen(process.env.PORT, (err) => {
     console.log(`server listening on port: ${process.env.PORT}`);
+});
+// Unhandled rejections
+process.on('unhandledRejection', err => {
+    server.close(() => {
+        process.exit(1);
+    });
 });
