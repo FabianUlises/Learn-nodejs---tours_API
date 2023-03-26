b845a9c940d21f55f8640579cdb1ed92bef7d7cf
FabianUlises <ulisesfab.car@outlook.com>
2023-03-26 09:47:18 -0700

    Updated sign in control handler to only accpect required fields

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,7 +3,12 @@ const User = require('./../models/userModel');
 exports.signUp = async(req, res) => {
     try {
         // Create upser from input
-        const user = await User.create(req.body);
+        const user = await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+            passwordConfirm: req.body.passwordConfirm
+        });
         res.status(201).json({
             status: 'success',
             data: {
