30af5e3f2e1339062c479a7f708b289680705bf0
FabianUlises <ulisesfab.car@outlook.com>
2023-03-27 09:02:42 -0700

    Installed and configured express-rate-limit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Dependencies
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 const AppError = require('./utils/appError');
 const cors = require('cors');
 const compression = require('compression');
@@ -15,6 +16,13 @@ app.options('*', cors());
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(compression());
+// Rate limiter config
+const limiter = rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000,
+    message: 'Too many requests from this IP, please try again in an hour'
+}); 
+app.use('/api', limiter);
 // Get requested time and date
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
